e55dc2fc69016b7dade4d733208b346576d7184b
emafla <emafla@users.noreply.github.com>
2025-03-02 11:55:21 -0500

    Update tallySheets.go
    Improved error handling
    Tally sheets created automatically

--- actas/apps-go/tallySheets.go
+++ actas/apps-go/tallySheets.go
@@ -105,62 +105,65 @@ func main() {
    contract := network.GetContract(chaincodeName)
    contracts := network.GetContract("qscc")
 
+   CrearActas(contract)
+   GrabarActas(contract)
+   // Create tally sheets. Init them with the fixed values. Save them "tallySheets.csv" file
+   fmt.Println("\n\n\n\n\n\n\n\n\n\n\n====================================================================")
+   fmt.Println("Creating tally sheets with their fixes values; saving them in \"tallySheets.csv\"")
+   fmt.Println("That file contains the UUID created for each tally sheet")
+   fmt.Println("The UUIDs are used to ID the tally sheets in the transactions below")
+
 
    fmt.Println("------------- Start of Simulator -------------")
 
    for true {
       fmt.Println("\n===============================")
-      fmt.Println("1.- Create tally sheets (must be executed first)")
-      fmt.Println("2.- Register a tally sheet image (stores TIFF image in IPFS)")
-      fmt.Println("3.- Invalidate a tally sheet")
-      fmt.Println("4.- Register results of a tally sheet")
-      fmt.Println("5.- Display a tally sheet's status")
-      fmt.Println("6.- Display election results")
-      fmt.Println("7.- Display a tally sheet' history/trace (saved as \"UUID-history.csv\", in TXs dir)")
-      fmt.Println("8.- Save all tally sheets to \"tallySheets.csv\"")
-      fmt.Println("9.- Display a transaction (given its TxId)")
-      fmt.Println("10.- Exit")
+      fmt.Println("1.- Register a tally sheet image (stores TIFF image in IPFS)")
+      fmt.Println("2.- Invalidate a tally sheet")
+      fmt.Println("3.- Register results of a tally sheet")
+      fmt.Println("4.- Display a tally sheet's status")
+      fmt.Println("5.- Display election results")
+      fmt.Println("6.- Save a tally sheet' history in \"./TXs/UUID-history.csv\")")
+      fmt.Println("7.- Save all tally sheets to \"tallySheets.csv\"")
+      fmt.Println("8.- Display a transaction (given its TxId)")
+      fmt.Println("9.- Exit")
       fmt.Print("\nPlease enter an option: ")
       var op int
       fmt.Scanf("%d", &op)
       switch op{
          case 1:
-            fmt.Println("--> Creating the tally sheets")
-            CrearActas(contract)
-
-         case 2:
             fmt.Println("--> Registering the CID of a tally sheet image")
             RegistrarActa(contract)
 
-         case 3:
+         case 2:
             fmt.Println("--> Invalidating a tally sheet")
             AnularActa(contract)
 
-         case 4:
+         case 3:
             fmt.Println("--> Registering results of a tally sheet")
             RegistrarResultados(contract)
 
-         case 5:
+         case 4:
             fmt.Println("--> Displaying a tally sheet's status")
             ConsultarActa(contract)
 
-         case 6:
+         case 5:
             fmt.Println("--> Computing Results")
             ConsultarResultados(contract)
 
-         case 7:
-	    fmt.Println("--> Saving a tally sheet history to file \"UUID-history.csv\"")
+         case 6:
+            fmt.Println("--> Saving a tally sheet history to file \"UUID-history.csv\"")
             ConsultarHistorial(contract)
 
-         case 8:
+         case 7:
             fmt.Println("--> Saving tally sheets to file \"tallySheets.csv\"")
             GrabarActas(contract)
 
-	 case 9:
+         case 8:
             fmt.Println("--> Displaying a transaction")
             ConsultarTx(contracts, network)
 
-         case 10:
+         case 9:
             fmt.Println("------------- End of Simulator -------------")
             os.Exit(0)
 
@@ -247,14 +250,14 @@ func CrearActas(contract *client.Contract) {
    }
    err = json.Unmarshal(byt, &actas)
    if err != nil {
-       panic(fmt.Errorf("failed to unmarshal the tally sheets. %v", err))
+       panic(fmt.Errorf("failed to unmarshal the tally sheets. %w", err))
    }
    for i := range actas {
       actas[i].Id = uuid.NewString()
    }
    actasJSON, err1 := json.Marshal(actas)
    if err1 != nil {
-        fmt.Println("Error encoding JSON:", err1)
+        panic(fmt.Errorf("Error encoding JSON: %w", err1))
         return
    }
 
@@ -269,11 +272,18 @@ func CrearActas(contract *client.Contract) {
 func RegistrarActa(contract *client.Contract) {
    var id, cid string
    id = getUUID()
+   if id == "" {
+      return
+   }
    cid = getCid()
+   if cid == "" {
+      return
+   }
 
    _, err := contract.SubmitTransaction("RegistrarActa", id, cid)
    if err != nil {
-      fmt.Errorf("failed to submit the transaction: %w", err)
+      fmt.Printf ("failed to submit the transaction: %v", err)
+      return
    }
 
    fmt.Println("Tally sheet image stored in IFPS and its CID has been registered")
@@ -282,10 +292,14 @@ func RegistrarActa(contract *client.Contract) {
 func AnularActa(contract *client.Contract) {
    var id string
    id = getUUID()
+   if id == "" {
+      return
+   }
 
    _, err := contract.SubmitTransaction("AnularActa", id)
    if err != nil {
-      panic(fmt.Errorf("failed to submit the transaction: %w", err))
+      fmt.Printf("failed to submit the transaction: %v", err)
+      return
    }
 
    fmt.Println("Tally sheet has been invalidates")
@@ -294,12 +308,15 @@ func AnularActa(contract *client.Contract) {
 func RegistrarResultados(contract *client.Contract) {
    var id, bl, nl, c1, c2, v string
    id = getUUID()
+   if id == "" {
+      return
+   }
    bl, nl, c1, c2, v = getResults()
-   //fmt.Printf ("Params: %s, %s, %s, %s, %s, %s\n", id, bl, nl, c1, c2, v);
 
    _, err := contract.SubmitTransaction("RegistrarResultados", id, bl, nl, c1, c2, v)
    if err != nil {
-      panic(fmt.Errorf("failed to submit the transaction: %w", err))
+      fmt.Printf("failed to submit the transaction: %v", err)
+      return
    }
    fmt.Println("Resultados have been registered")
 }
@@ -310,7 +327,8 @@ func ConsultarResultados(contract *client.Contract) {
    var result []byte
    result, err := contract.EvaluateTransaction("ListarActas")
    if err != nil {
-      panic(fmt.Errorf("failed to submit the transaction: %w", err))
+      fmt.Printf ("failed to submit the transaction: %v", err)
+      return
    }
    computeResults(result)
   }
@@ -319,20 +337,27 @@ func ConsultarResultados(contract *client.Contract) {
 func ConsultarActa(contract *client.Contract) {
    var id string
    id = getUUID()
+   if id == "" {
+      return
+   }
 
    evaluateResult, err := contract.EvaluateTransaction("ConsultarActa", id)
    if err != nil {
-      panic(fmt.Errorf("failed to submit the transaction: %w", err))
+      fmt.Printf("failed to submit the transaction: %v", err)
    }
- 
+
    result := formatJSON(evaluateResult)
+   if result == "" {
+      return
+   }
 
    fmt.Printf("Tally Sheet %s:\n%s\n", id, result)
 
    // CID of the acta file
    var acta Acta
    if err := json.Unmarshal([]byte(result), &acta); err != nil {
-      fmt.Errorf("failed to unmarshal JSON string: %v", err)
+      fmt.Printf("failed to unmarshal JSON string: %v", err)
+      return
     }
    cid := acta.Cid
 
@@ -343,21 +368,24 @@ func ConsultarActa(contract *client.Contract) {
       // Create a local file to save the retrieved content
       outFile, err := os.Create("./tallySheetsCID/" + cid + ".tif")
       if err != nil {
-         fmt.Errorf("failed to create local IPFS file for tally sheet image: %v", err)
+         fmt.Printf("failed to create local IPFS file for tally sheet image: %v", err)
+         return
       }
       defer outFile.Close()
 
       // Retrieve the file from IPFS
       reader, err := sh.Cat(cid)
       if err != nil {
-         fmt.Errorf("failed to retrieve tally sheet image file from IPFS: %v", err)
+         fmt.Printf("failed to retrieve tally sheet image file from IPFS: %v", err)
+         return
       }
       defer reader.Close()
 
       // Copy the content to the local file
       _, err = io.Copy(outFile, reader)
       if err != nil {
-         fmt.Errorf("failed to copy tally sheet from IPFS to local file: %v", err)
+         fmt.Printf ("failed to copy tally sheet from IPFS to local file: %v", err)
+         return
       }
 
       fmt.Println("Tally sheet copied from IPFS to local file: %s", "tallySheetsCID/"+cid+".tif")
@@ -369,19 +397,25 @@ func ConsultarHistorial(contract *client.Contract) {
    var result []byte
    var id string
    id = getUUID()
+   if id == "" {
+      return
+   }
 
    result, err := contract.SubmitTransaction("HistorialActa", id)
    if err != nil {
-      panic(fmt.Errorf("failed to evaluate transaction: %w", err))
+      fmt.Printf("failed to evaluate transaction: %v", err)
+      return
    }
 
    err = json.Unmarshal(result, &records)
    if err != nil {
-      panic(fmt.Errorf("failed to unmarshal JSON to slice: %w", err))
+      fmt.Printf("failed to unmarshal JSON to slice: %v", err)
+      return
    }
 
    csvFile, err1 := os.Create("./TXs/" + id + "-history.csv")
    if err1 != nil {
+      fmt.Printf("failed to create file: %v", err1)
        return
    }
    defer csvFile.Close()
@@ -398,12 +432,12 @@ func ConsultarHistorial(contract *client.Contract) {
 
    for _, r := range records {
       var csvRow []string
-      csvRow = append(csvRow, r.Record.Id, fmt.Sprint(r.Record.Cda), 
-         fmt.Sprint(r.Record.Cargo), fmt.Sprint(r.Record.Provincia), 
-	 fmt.Sprint(r.Record.Canton), fmt.Sprint(r.Record.Parroquia),
-         fmt.Sprint(r.Record.Junta), fmt.Sprint(r.Record.Status), 
-	 fmt.Sprint(r.Record.Electores), fmt.Sprint(r.Record.Blancos), 
-	 fmt.Sprint(r.Record.Nulos), fmt.Sprint(r.Record.Candidato1),
+      csvRow = append(csvRow, r.Record.Id, fmt.Sprint(r.Record.Cda),
+         fmt.Sprint(r.Record.Cargo), fmt.Sprint(r.Record.Provincia),
+         fmt.Sprint(r.Record.Canton), fmt.Sprint(r.Record.Parroquia),
+         fmt.Sprint(r.Record.Junta), fmt.Sprint(r.Record.Status),
+         fmt.Sprint(r.Record.Electores), fmt.Sprint(r.Record.Blancos),
+         fmt.Sprint(r.Record.Nulos), fmt.Sprint(r.Record.Candidato1),
          fmt.Sprint(r.Record.Candidato2), fmt.Sprint(r.Record.Votos), r.Record.Cid,
          r.TxId, fmt.Sprint(r.Timestamp), fmt.Sprint(r.IsDelete))
       if err = writer.Write(csvRow); err != nil {
@@ -418,22 +452,19 @@ func GrabarActas(contract *client.Contract) {
    var actas []Acta
    evaluateResult, err := contract.EvaluateTransaction("ListarActas")
    if err != nil {
-      panic(fmt.Errorf("failed to submit the transaction: %w", err))
+      fmt.Printf("failed to submit the transaction: %v", err)
+      return
    }
-
-   //result := formatJSON(evaluateResult)
-
-   //fmt.Printf("Actas:\n%s\n", result)
-   //fmt.Printf("Format: %T\n", evaluateResult)
 
    err = json.Unmarshal(evaluateResult, &actas)
    if err != nil {
-      panic(fmt.Errorf("failed to unmarshal JSON to TallySheet slice: %w", err))
+      fmt.Printf ("failed to unmarshal JSON to TallySheet slice: %v", err)
+      return
    }
 
    csvFile, err1 := os.Create("tallySheets.csv")
    if err1 != nil {
-       return 
+       return
    }
    defer csvFile.Close()
 
@@ -465,16 +496,21 @@ func GrabarActas(contract *client.Contract) {
 func ConsultarTx(contracts *client.Contract, network *client.Network) {
    var txId string
    txId = getTxId()
+   if txId == "" {
+      return
+   }
 
    txBytes, err := contracts.EvaluateTransaction("GetTransactionByID", network.Name(), txId);
    if err != nil {
-      panic(fmt.Errorf("failed to evaluate transaction: %w", err))
+      fmt.Printf ("failed to get transaction: %v", err)
+      return
    }
 
    var processedTx peer.ProcessedTransaction
    err = proto.Unmarshal(txBytes, &processedTx)
    if err != nil {
-      panic(fmt.Errorf("failed to unmarshal ProcessedTransaction: %w", err))
+      fmt.Printf ("failed to unmarshal ProcessedTransaction: %v", err)
+      return
    }
    fmt.Printf ("valCode: %s\n", peer.TxValidationCode_name[processedTx.ValidationCode])
 
@@ -482,12 +518,14 @@ func ConsultarTx(contracts *client.Contract, network *client.Network) {
    var payload common.Payload
    err = proto.Unmarshal(envelope.Payload, &payload)
    if err != nil {
-      panic(fmt.Errorf("failed to unmarshal Payload: %w", err))
+      fmt.Printf("failed to unmarshal Payload: %v", err)
+      return
    }
    var ccHeader common.ChannelHeader
    err = proto.Unmarshal(payload.Header.ChannelHeader, &ccHeader)
    if err != nil {
-      panic(fmt.Errorf("failed to unmarshal ChannelHeader: %w", err))
+      fmt.Printf("failed to unmarshal ChannelHeader: %v", err)
+      return
    }
    fmt.Printf("chHdrType: %s\nVersion: %d\nTS: %s\nccID: %s\nTxId: %s\nEp: %d\n",
       common.HeaderType_name[ccHeader.Type], ccHeader.Version, ccHeader.Timestamp.AsTime().Format(time.RFC1123Z), ccHeader.ChannelId, ccHeader.TxId, ccHeader.Epoch)
@@ -499,7 +537,8 @@ func ConsultarTx(contracts *client.Contract, network *client.Network) {
    var signHeader common.SignatureHeader
    err = proto.Unmarshal(payload.Header.SignatureHeader, &signHeader)
    if err != nil {
-      panic(fmt.Errorf("failed to unmarshal SignatureHeader: %w", err))
+      fmt.Printf("failed to unmarshal SignatureHeader: %v", err)
+      return
    }
 
    id, err := deserializeIdentity(signHeader.Creator)
@@ -523,6 +562,7 @@ func ConsultarTx(contracts *client.Contract, network *client.Network) {
    err = decodeTransactionPayload(envelope.Payload)
    if err != nil {
       fmt.Println("Error decoding transaction payload:", err)
+      return
    }
 
    return
@@ -533,30 +573,39 @@ func ConsultarTx(contracts *client.Contract, network *client.Network) {
 func formatJSON(data []byte) string {
    var prettyJSON bytes.Buffer
    if err := json.Indent(&prettyJSON, data, "", "  "); err != nil {
-      panic(fmt.Errorf("failed to parse JSON: %w", err))
+      fmt.Printf ("\n*** failed to parse JSON: %v", err)
+      return ""
    }
    return prettyJSON.String()
 }
 
 // Obtener ID de acta
 func getUUID() (string){
-   var uuid string
-   fmt.Print("Enter tally sheet UUID (copy from \"tallySheet.csv\" file): ")
-   fmt.Scanf("%s", &uuid)
-   return uuid
+   var uuidS string
+   fmt.Print("Enter tally sheet UUID (get from \"tallySheets.csv\" file): ")
+   fmt.Scanf("%s", &uuidS)
+   if err := uuid.Validate(uuidS); err != nil {
+      fmt.Printf ("\n*** Please, enter a valid UUID")
+      return ""
+   }
+   return uuidS
 }
 
 // Obtener TXID de transaccion
 func getTxId() (string){
    var txid string
-   fmt.Print("Enter transaction ID (from \"UUID-history.csv\" file): ")
+   fmt.Print("Enter transaction ID (get from \"UUID-history.csv\" file): ")
    fmt.Scanf("%s", &txid)
+   if len(txid) != 64 {
+      fmt.Printf("\n*** Please, enter a valid TxID")
+      return ""
+   }
    return txid
 }
 
 func getCid() (string){
    var fname string
-   fmt.Print("Enter tally sheet image file name: ")
+   fmt.Print("Enter tally sheet image file name (e.g. tallySheets/A...tif): ")
    fmt.Scanf("%s", &fname)
 
    sh := shell.NewShell("localhost:5001")
@@ -564,14 +613,16 @@ func getCid() (string){
    // Read the file you want to add
    file, err := os.Open(fname)
    if err != nil {
-      panic(fmt.Errorf("failed to open tally sheet TIF file: %w", err))
+      fmt.Printf ("\n*** failed to open tally sheet TIF file: %v", err)
+      return ""
    }
    defer file.Close()
 
     // Add the file to IPFS
    cid, err := sh.Add(file)
    if err != nil {
-      panic(fmt.Errorf("failed to add file to IPFS: %w", err))
+      fmt.Printf("\n*** failed to add file to IPFS: %v", err)
+      return ""
    }
 
    return cid
@@ -597,7 +648,8 @@ func computeResults(result []byte){
    var actas []Acta
    err := json.Unmarshal(result, &actas)
    if err != nil {
-      panic(fmt.Errorf("failed to unmarshal JSON to TallySheet slice: %w", err))
+      fmt.Printf("\n*** failed to unmarshal JSON to TallySheet slice: %v", err)
+      return
    }
 
    tElect := 0
@@ -639,7 +691,7 @@ func deserializeIdentity(serialized []byte) (*msp.SerializedIdentity, error) {
    identity := &msp.SerializedIdentity{}
    err := proto.Unmarshal(serialized, identity)
    if err != nil {
-      return nil, fmt.Errorf("failed to unmarshal SerializedIdentity: %v", err)
+      return nil, fmt.Errorf("\n*** failed to unmarshal SerializedIdentity: %w", err)
    }
    return identity, nil
 }
@@ -647,12 +699,12 @@ func deserializeIdentity(serialized []byte) (*msp.SerializedIdentity, error) {
 func parseCertificate(identity *msp.SerializedIdentity) (*x509.Certificate, error) {
    block, _ := pem.Decode(identity.IdBytes)
    if block == nil {
-      return nil, fmt.Errorf("failed to decode PEM block from identity bytes")
+      return nil, fmt.Errorf("\n*** failed to decode PEM block from identity bytes")
    }
 
    cert, err := x509.ParseCertificate(block.Bytes)
    if err != nil {
-      return nil, fmt.Errorf("failed to parse x509 certificate: %v", err)
+      return nil, fmt.Errorf("\n*** failed to parse x509 certificate: %w", err)
    }
    return cert, nil
 }
@@ -663,14 +715,14 @@ func decodeTransactionPayload(payloadBytes []byte) error {
    payload := &common.Payload{}
    err := proto.Unmarshal(payloadBytes, payload)
    if err != nil {
-      return fmt.Errorf("failed to unmarshal payload: %v", err)
+      return fmt.Errorf("\n*** failed to unmarshal payload: %w", err)
    }
 
    // Extract Channel Header
    header := &common.ChannelHeader{}
    err = proto.Unmarshal(payload.Header.ChannelHeader, header)
    if err != nil {
-      return fmt.Errorf("failed to unmarshal channel header: %v", err)
+      return fmt.Errorf("\n*** failed to unmarshal channel header: %w", err)
    }
 
    fmt.Println("Channel ID:", header.ChannelId)
@@ -680,7 +732,7 @@ func decodeTransactionPayload(payloadBytes []byte) error {
    transaction := &peer.Transaction{}
    err = proto.Unmarshal(payload.Data, transaction)
    if err != nil {
-      return fmt.Errorf("failed to unmarshal transaction: %v", err)
+      return fmt.Errorf("\n*** failed to unmarshal transaction: %w", err)
    }
 
    // Process each Action in the transaction
@@ -689,21 +741,21 @@ func decodeTransactionPayload(payloadBytes []byte) error {
       chaincodeActionPayload := &peer.ChaincodeActionPayload{}
       err = proto.Unmarshal(action.Payload, chaincodeActionPayload)
       if err != nil {
-         return fmt.Errorf("failed to unmarshal chaincode action payload: %v", err)
+         return fmt.Errorf("\n*** failed to unmarshal chaincode action payload: %w", err)
       }
 
       // Extract ProposalResponsePayload
       proposalResponsePayload := &peer.ProposalResponsePayload{}
       err = proto.Unmarshal(chaincodeActionPayload.Action.ProposalResponsePayload, proposalResponsePayload)
       if err != nil {
-         return fmt.Errorf("failed to unmarshal proposal response payload: %v", err)
+         return fmt.Errorf("\n*** failed to unmarshal proposal response payload: %w", err)
       }
 
       // Extract ChaincodeAction
       chaincodeAction := &peer.ChaincodeAction{}
       err = proto.Unmarshal(proposalResponsePayload.Extension, chaincodeAction)
       if err != nil {
-         return fmt.Errorf("failed to unmarshal chaincode action: %v", err)
+         return fmt.Errorf("\n*** failed to unmarshal chaincode action: %w", err)
       }
 
       // Print Chaincode Response
@@ -715,7 +767,7 @@ func decodeTransactionPayload(payloadBytes []byte) error {
       readWriteSet := &rwset.TxReadWriteSet{}
       err = proto.Unmarshal(chaincodeAction.Results, readWriteSet)
       if err != nil {
-         return fmt.Errorf("failed to unmarshal read-write set: %v", err)
+         return fmt.Errorf("\n*** failed to unmarshal read-write set: %w", err)
       }
 
       rwSetJSON, _ := json.MarshalIndent(readWriteSet, "", "  ")
@@ -725,4 +777,3 @@ func decodeTransactionPayload(payloadBytes []byte) error {
 
    return nil
 }
-
